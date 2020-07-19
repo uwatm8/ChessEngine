899d2017fdb88829af0413d7dd3e81dfcb6eedaa
ArvidHast <hasta@student.chalmers.se>
2020-07-19 17:18:46 +0200

    Update value function and start tree search

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import chess
 import chess.pgn
+import math
+from collections.abc import Mapping
 from random import randrange
 
 board = chess.Board()
 legalMoves = []
 
 maxMoves = 1000
-maxGames = 10
+maxGames = 1
 
 def getLegalMoves(board):
     legalMoves = []
@@ -17,6 +19,9 @@ def getLegalMoves(board):
 
 def split(word):
     return [char for char in word]
+
+def isDict(variable):
+    return isinstance(variable, Mapping)
 
 currentDepth = 0
 movesMade = 0
@@ -60,36 +65,65 @@ def getScorePieces(board):
         # print("CAN CLAIM DRAW")
         return 0
 
+    i = 0
+
     for char in chars:
+        if char == ' ' or char == '\n':
+            continue
+
+        row = int(i/8)
+        column = i%8
+        i += 1
+
+        distanceToCenter = math.sqrt((row-3.5)**2 + (column-3.5)**2)
+
+        positionValue = -(distanceToCenter/1000000.0)
+
+        #print("distance", multiplier)
+
 
         # Black pieces
         if char == "r":
             score -= 5
+            score -= positionValue
         elif char == "n":
             score -= 2.9
+            score -= positionValue
         elif char == "q":
             score -= 9
+            score -= positionValue
         elif char == "b":
             score -= 3
+            score -= positionValue
         elif char == "k":
             score -= 1000
+            score += positionValue
         elif char == "p":
             score -= 1
+            score -= positionValue
 
         # White pieces
         elif char == "R":
             score += 5
+            score += positionValue
         elif char == "N":
             score += 2.9
+            score += positionValue
         elif char == "Q":
             score += 9
+            score += positionValue
         elif char == "B":
             score += 3
+            score += positionValue
         elif char == "K":
             score += 1000
+            score -= positionValue
         elif char == "P":
             score += 1
+            score += positionValue
 
+    #print(board)
+    #print(score)
     return score
 
 def getMoveRandom(board):
@@ -133,6 +167,101 @@ def getBestMove1Depth(board):
         resetBoardX(board, 1)
 
     return bestMove
+
+def getScoreAfterMove(board, move):
+    move = str(move)
+    makeBoardMove(board, move)
+    score = getScorePieces(board)
+
+    resetBoardX(board, 1)
+
+    return score
+
+def getBestMoveSearchTree(board, nNodes):
+
+    bestMove = getMoveRandom(board)
+
+    scores = {}
+
+    searchedNodes = 0
+
+    boardToSearch = board
+
+    queue = []
+
+    currentDepth = 0
+
+    legalMoves = getLegalMoves(board)
+
+    if len(legalMoves) == 0:
+        return ""
+
+    for move in legalMoves:
+        scores[move] = getScoreAfterMove(board, move)
+
+    sortedScores = sorted(scores.items(), key=lambda x: x[1])
+    scores = sortedScores
+
+    # always start from current board state
+    while(searchedNodes < nNodes):
+        searchedNodes += 1
+
+        if board.turn == chess.WHITE:
+            bestMove = str(sortedScores[len(sortedScores)-1][0])
+
+            pass
+        elif board.turn == chess.BLACK:
+            bestMove = str(sortedScores[0][0])
+
+            pass
+
+
+        #sortedScores = sorted(scores.items(), key=lambda x: x[1])
+        #scores = sortedScores
+
+
+
+
+        if False:
+            for tuple in scores:
+                move = tuple[0]
+                score = tuple[1]
+
+                for i in range(len(sortedScores)):
+                    queue.append(move)
+
+
+
+            boardToSearch = makeBoardMove(bestMove)
+
+
+
+    #sortedScores = sorted(scores.items(), key=lambda x: x[1])
+
+    if board.turn == chess.WHITE:
+        # take last element "highest score"
+        bestMove = str(sortedScores[len(sortedScores)-1][0])
+    elif board.turn == chess.BLACK:
+        # take first element "lowest score"
+        bestMove = str(sortedScores[0][0])
+
+
+    scores = sortedScores
+
+    print("sorted:", sortedScores)
+
+
+
+    if False:
+        for key in scores:
+            print("score", key, " : ", scores[key])
+
+        for i in sorted (scores):
+            print("move :", scores[i])
+            print("moveI:", i)
+
+    return bestMove
+
 
 def getBestMove2Depth(board):
 
@@ -329,6 +458,8 @@ def getBestMoveDepth2(board):
 
 
 
+
+
 def getBestMoveDepth3(board):
 
     #print("iteration@@@@@@@@@@@@@@@@@@@@@@@@@")
@@ -416,16 +547,15 @@ for g in range(maxGames):
             break
 
 
-        # Make moves
-        legalMoves = getLegalMoves(board)
-
         move = ""
         if board.turn == chess.WHITE:
             #move = getBestMove2Depthv3(board)
-            move = getBestMove3Depth(board)
+            move = getMoveRandom(board)
+            #move = getBestMove2Depth(board)
 
         if board.turn == chess.BLACK:
-            move = getMoveRandom(board)
+            move = getBestMoveSearchTree(board, 1)
+            #move = getBestMove3Depth(board)
 
         makeBoardMove(board, move)
 
@@ -434,6 +564,8 @@ for g in range(maxGames):
         #print("moves made: ", movesMade)
 
         #board.push(chess.Move.from_uci(move))
+
+        print("trying move", move)
 
         if m == 0:
             node = savedGame.add_main_variation(chess.Move.from_uci(move))
