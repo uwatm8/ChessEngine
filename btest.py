b30557800a254ddbbffe4d7b94da90418698358f
ArvidHast <hasta@student.chalmers.se>
2020-09-27 01:04:34 +0200

    basic board state stream working

--- btest.py
+++ btest.py
@@ -1,4 +1,6 @@
 import berserk
+import time
+import itertools
 client = berserk.Client()
 
 upgradeToBot = False
@@ -13,19 +15,68 @@ with open('.apitoken') as f:
 session = berserk.TokenSession("" + token)
 client = berserk.Client(session)
 
+gameRunning = False
+
+
+
+
+
+
 def should_accept(arg):
-    return True
+    return not gameRunning
 
 
 
-
-is_polite = True
+should_decline_if_in_game = True
 for event in client.bots.stream_incoming_events():
+    print("asdasd")
     if event['type'] == 'challenge':
+        print("event--------------------------------")
+        print(event)
+        print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
         if should_accept(event):
             client.bots.accept_challenge(event['challenge']['id'])
-        elif is_polite:
+            gameID = event['challenge']['id']
+            print("starting challange")
+            gameRunning = True
+            break
+        elif should_decline_if_in_game:
             client.bots.decline_challenge(event['challenge']['id'])
-        elif event['type'] == 'gameStart':
-            game = Game(event['id'])
-            game.start()
+            print("declining challange")
+        #elif event['type'] == 'gameStart':
+        #    game = Game(event['id'])
+        #    game.start()
+        #    gameRunning = True
+        #    print("starting broken game")
+
+print("done with loop")
+client.bots.make_move(gameID, 'e2e3')
+
+if not gameRunning:
+    print("setting custom id @@@@@@@@@@@@@@@@@@@@@@")
+    gameID = "vztNjpfq"
+            
+print("gameid: ", gameID)
+
+for i in range(10):
+    print("looping 2")
+    print("looping 3")
+    
+
+    gen = client.bots.stream_game_state(gameID)
+    print("looping 4")
+
+    for element in gen:
+        print("looping in gen")    
+        if(list(element)):
+            print("looping if")
+            print("element: ",element)
+        else:
+            print("no board")
+
+    
+    print("looping 5")
+print("done with loop2")
+
+    
